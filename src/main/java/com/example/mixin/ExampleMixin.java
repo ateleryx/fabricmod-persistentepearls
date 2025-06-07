8cbb1284edcc64235e69ab2e2105510ccc0591bd
ateleryx <mateojpaula@gmail.com>
2025-06-07 19:40:38 -0400

    Update ExampleMixin.java

--- src/main/java/com/example/mixin/ExampleMixin.java
+++ src/main/java/com/example/mixin/ExampleMixin.java
@@ -1,15 +1,31 @@
-package com.example.mixin;
+package nl.enjarai.persistentpearls.mixin;
 
-import net.minecraft.server.MinecraftServer;
+import net.minecraft.entity.EntityType;
+import net.minecraft.entity.projectile.thrown.EnderPearlEntity;
+import net.minecraft.entity.projectile.thrown.ThrownItemEntity;
+import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(MinecraftServer.class)
-public class ExampleMixin {
-	@Inject(at = @At("HEAD"), method = "loadWorld")
-	private void init(CallbackInfo info) {
-		// This code is injected into the start of MinecraftServer.loadWorld()V
-	}
+import static nl.enjarai.persistentpearls.PersistentPearls.PEARLS_PERSISTENT_GAMERULE;
+
+@Mixin(EnderPearlEntity.class)
+public abstract class EnderPearlEntityMixin extends ThrownItemEntity {
+    public EnderPearlEntityMixin(EntityType<? extends ThrownItemEntity> entityType, World world) {
+        super(entityType, world);
+    }
+
+    @Inject(
+            method = "Lnet/minecraft/entity/projectile/thrown/EnderPearlEntity;tick()V",
+            at = @At("HEAD"),
+            cancellable = true
+    )
+    private void persistentPearls$disablePearlDespawn(CallbackInfo ci) {
+        if (getWorld().getGameRules().getBoolean(PEARLS_PERSISTENT_GAMERULE)) {
+            super.tick();
+            ci.cancel();
+        }
+    }
 }
