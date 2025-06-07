eafa84d20a1c136d9072f3834b2ca17adb21b406
ateleryx <mateojpaula@gmail.com>
2025-06-07 19:40:19 -0400

    Update ExampleMod.java

--- src/main/java/com/example/ExampleMod.java
+++ src/main/java/com/example/ExampleMod.java
@@ -1,24 +1,20 @@
-package com.example;
+package nl.enjarai.persistentpearls;
 
 import net.fabricmc.api.ModInitializer;
 
+import net.fabricmc.fabric.api.gamerule.v1.GameRuleFactory;
+import net.fabricmc.fabric.api.gamerule.v1.GameRuleRegistry;
+import net.minecraft.world.GameRules;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class ExampleMod implements ModInitializer {
-	public static final String MOD_ID = "modid";
-
-	// This logger is used to write text to the console and the log file.
-	// It is considered best practice to use your mod id as the logger's name.
-	// That way, it's clear which mod wrote info, warnings, and errors.
-	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+public class PersistentPearls implements ModInitializer {
+	public static final Logger LOGGER = LoggerFactory.getLogger("persistent-pearls");
+	public static final GameRules.Key<GameRules.BooleanRule> PEARLS_PERSISTENT_GAMERULE =
+			GameRuleRegistry.register("persistentPearls", GameRules.Category.UPDATES,
+					GameRuleFactory.createBooleanRule(true));
 
 	@Override
 	public void onInitialize() {
-		// This code runs as soon as Minecraft is in a mod-load-ready state.
-		// However, some things (like resources) may still be uninitialized.
-		// Proceed with mild caution.
-
-		LOGGER.info("Hello Fabric world!");
 	}
 }
